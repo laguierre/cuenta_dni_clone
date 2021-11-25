12c7db41e8811485ccf8ae8896a2976bc4e58d47
laguierre <laguierre@gmail.com>
2021-11-25 20:50:33 -0300

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'src/pages/home_page/home_page.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Cuenta DNI Clone',
       home: HomePage(),
